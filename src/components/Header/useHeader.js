68ba3e636811f35d29c52a1e7edf15187175a088
andrelcalado <andrelcalad@hotmail.com>
2025-04-01 14:41:37 -0300

    :bug: [FIX] Header logout redirect

--- src/components/Header/useHeader.js
+++ src/components/Header/useHeader.js
@@ -1,12 +1,15 @@
 "use client";
 
 import useAuthStore from '@/store/user';
+import { useRouter } from 'next/navigation';
 
 const useHeader = () => {
   const { logout } = useAuthStore();
+  const router = useRouter();
 
   const handleLogout = () => {
     logout();
+    router.push('/login');
   }
     
   return {
